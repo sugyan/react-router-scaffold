cd64dbd01783fe9aca6361096d6f5b4c12dccf36
sugyan <sugi1982@gmail.com>
2015-04-07 00:57:05 +0900

    add created_at, updated_at

--- app/assets/javascripts/components/book_index.js.jsx
+++ app/assets/javascripts/components/book_index.js.jsx
@@ -36,6 +36,8 @@ class BookIndexTable extends React.Component {
                         <th>ID</th>
                         <th>Title</th>
                         <th>Price</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
                         <th colSpan="3"></th>
                     </tr>
                 </thead>
@@ -68,6 +70,8 @@ class BookIndexTableRow extends React.Component {
                 <td>{this.props.book.id}</td>
                 <td>{this.props.book.title}</td>
                 <td>{this.props.book.price}</td>
+                <td>{this.props.book.created_at}</td>
+                <td>{this.props.book.updated_at}</td>
                 <td><ReactRouter.Link to="show" params={{ id: this.props.book.id }}>Show</ReactRouter.Link></td>
                 <td><ReactRouter.Link to="edit" params={{ id: this.props.book.id }}>Edit</ReactRouter.Link></td>
                 <td><a href="" onClick={this.handleClickDestroy.bind(this, this.props.book.url)}>Destroy</a></td>
--- app/views/books/index.json.jbuilder
+++ app/views/books/index.json.jbuilder
@@ -1,4 +1,6 @@
 json.array!(@books) do |book|
   json.extract! book, :id, :title, :price
+  json.created_at book.created_at.strftime('%Y-%m-%d %H:%M')
+  json.updated_at book.updated_at.strftime('%Y-%m-%d %H:%M')
   json.url book_url(book, format: :json)
 end
